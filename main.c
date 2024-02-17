86fc0a28cab65756ee624b1ef52fd6b6f4d6ac97
dongyeuk <yupab3@naver.com>
2024-02-17 12:58:32 +0900

    [norm] erase empty lines

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 19:26:12 by jaejilee          #+#    #+#             */
-/*   Updated: 2024/02/16 20:35:10 by jaejilee         ###   ########.fr       */
+/*   Updated: 2024/02/17 12:57:54 by dongyeuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,5 @@ int main(int argc, char *argv[])
 		write(2, "Error\nminiRT: wrong argument count\n", 35);
 		return (1);
 	}
-	
-
 	return (0);
 }
