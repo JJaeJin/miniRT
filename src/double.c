d7d7a165fa162a0797ba633a9cddd64e7475d47f
dongyeuk <yupab3@naver.com>
2024-02-20 16:52:50 +0900

    [mod] cylinder height limit

--- src/double.c
+++ src/double.c
@@ -3,41 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   double.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 15:19:09 by jaejilee          #+#    #+#             */
-/*   Updated: 2024/02/20 16:19:49 by jaejilee         ###   ########.fr       */
+/*   Updated: 2024/02/20 16:43:08 by dongyeuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "argument.h"
-
-int	is_double(char *s, double *f)
-{
-	int	i;
-
-	if (ft_isdigit(s[0]) == FALSE)
-		return (FALSE);
-	i = 0;
-	while (ft_isdigit(s[i]) == TRUE)
-		i++;
-	if (s[i] != '.' && s[i] != 0)
-		return (FALSE);
-	if (s[i] == '.')
-	{
-		i++;
-		if (ft_isdigit(s[i]) == FALSE)
-			return (FALSE);
-		while (ft_isdigit(s[i]) == TRUE)
-			i++;
-		if (s[i] != 0)
-			return (FALSE);
-	}
-	if (ft_atod(s + i, f) == FALSE)
-		return (FALSE);
-	return (TRUE);
-}
 
 int	is_unsigned_double(char *s, double *f)
 {
@@ -60,7 +34,7 @@ int	is_unsigned_double(char *s, double *f)
 		if (s[i] != 0)
 			return (FALSE);
 	}
-	if (ft_atod(s + i, f) == FALSE \
+	if (ft_atod(s, f) == FALSE \
 		|| f <= 0)
 		return (FALSE);
 	return (TRUE);
