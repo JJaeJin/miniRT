c1e5d1fe850c1f1d9976e963e42c13e7889be9d6
dongyeuk <yupab3@naver.com>
2024-03-13 21:01:50 +0900

    [mod] fix ambient func

--- src/draw_ambient.c
+++ src/draw_ambient.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_ambient.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 10:35:19 by jaejilee          #+#    #+#             */
-/*   Updated: 2024/02/27 12:22:58 by jaejilee         ###   ########.fr       */
+/*   Updated: 2024/03/13 21:01:31 by dongyeuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 
 void	apply_ambient(t_color *c, t_ambient_lightning *amb)
 {
-	c->red *= amb->color.red * amb->ratio;
+	c->red *= amb->color.red * amb->ratio / 255;
 	if (c->red > 255)
 		c->red = 255;
-	c->green *= amb->color.green * amb->ratio;
+	c->green *= amb->color.green * amb->ratio / 255;
 	if (c->green > 255)
 		c->green = 255;
-	c->blue *= amb->color.blue * amb->ratio;
+	c->blue *= amb->color.blue * amb->ratio / 255;
 	if (c->blue > 255)
 		c->blue = 255;
 }
